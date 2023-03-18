c8ef1282246d2b94a998aef5a6e848367a719fb8
Gian Marco Pablo Totocayo <gianmarcopablo588@gmail.com>
2023-03-17 20:23:07 -0500

    fin

--- js/app.js
+++ js/app.js
@@ -7,48 +7,45 @@ const er = /^(([^<>()\[\]\\.,;:\s@\"]+(\.[^<>()\[\]\\.,;:\s@\"]+)*)|(\".+\"))@((
 const btnReset = document.querySelector("#resetBtn")
 
 
-eventListener()
-function eventListener(){
-    document.addEventListener("DOMContentLoaded",inicarApp)
+cargarEventListeners()
+function cargarEventListeners(){
+    document.addEventListener("DOMContentLoaded",iniciarApp)
     email.addEventListener("blur",validarFormulario)
     asunto.addEventListener("blur",validarFormulario)
     mensaje.addEventListener("blur",validarFormulario)
     formulario.addEventListener("submit",enviarEmail)
-
     btnReset.addEventListener("click",function(e){
         e.preventDefault()
         resetearFormulario()
     })
 }
 
-function inicarApp(){
+function iniciarApp(){
     btnEnviar.disabled = true
     btnEnviar.classList.add("cursor-not-allow","opacity-50")
 }
 
 function validarFormulario(e){
-
-    if(e.target.value.length > 0){
-
+    if(e.target.value.length > 1){
         const error = document.querySelector(".error")
         if(error){
             error.remove()
         }
         e.target.classList.remove("border","border-red-500")
         e.target.classList.add("border","border-green-500")
-
     }else{
         e.target.classList.remove("border","border-green-500")
         e.target.classList.add("border","border-red-500")
         mostrarError("Todos los campos son obligatorios")
     }
-    
     if(e.target.type === "email"){
         if(er.test(e.target.value)){
             const error = document.querySelector(".error")
             if(error){
-            error.remove()
+                error.remove()
             }
+            e.target.classList.remove("border","border-red-500")
+            e.target.classList.add("border","border-green-500")
         }else{
             e.target.classList.remove("border","border-green-500")
             e.target.classList.add("border","border-red-500")
@@ -58,43 +55,40 @@ function validarFormulario(e){
 
     if(er.test(email.value) && asunto.value !== "" && mensaje.value !== ""){
         btnEnviar.disabled = false
-        btnEnviar.classList.remove("cursor-not-allow","opacity-50") 
+        btnEnviar.classList.remove("cursor-not-allow","opacity-50")
     }
-    
 }
 
 function mostrarError(mensaje){
     const mensajeError = document.createElement("p")
     mensajeError.textContent = mensaje
     mensajeError.classList.add("border","border-red-500","bg-red-300","text-black","p-3","text-center","mt-5","error","font-bold","uppercase")
-
-    const errores = document.querySelector(".error")
-    if(!errores){
+    const errores = document.querySelectorAll(".error")
+    if(errores.length === 0){
         formulario.appendChild(mensajeError)
     }
 }
 
 function enviarEmail(e){
     e.preventDefault()
-
     const spinner = document.querySelector("#spinner")
     spinner.style.display = "flex"
 
-    setTimeout(()=>{
-        spinner.style.display  = "none"
+    setTimeout(() => {
+        spinner.style.display = "none"
         const parrafo = document.createElement("p")
-        parrafo.textContent = "Mensaje enviado correctamente"
-        parrafo.classList.add("text-center","ny-10","p-3","bg-green-500", "text-white","uppercase","font-bold")
+        parrafo.textContent = "Mensaje Enviado Correctamente"
+        parrafo.classList.add("border","border-green-500","bg-green-300","text-black","p-3","text-center","mt-5","error","font-bold","uppercase")
         formulario.appendChild(parrafo)
-
-        setTimeout(()=>{
+        setTimeout(() => {
             parrafo.remove()
             resetearFormulario()
-        },2000)
-    },3000)
+        }, 3000);
+    }, 3000);
+    
 }
 
 function resetearFormulario(){
     formulario.reset()
-    inicarApp()
+    iniciarApp()
 }
