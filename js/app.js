9857eac61ebb49481349ce5fdcd4179054bf6947
Gian Marco Pablo Totocayo <gianmarcopablo588@gmail.com>
2023-03-18 22:29:09 -0500

    actualizacion

--- js/app.js
+++ js/app.js
@@ -6,7 +6,6 @@ const formulario = document.querySelector("#enviar-mail")
 const er = /^(([^<>()\[\]\\.,;:\s@\"]+(\.[^<>()\[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 const btnReset = document.querySelector("#resetBtn")
 
-
 cargarEventListeners()
 function cargarEventListeners(){
     document.addEventListener("DOMContentLoaded",iniciarApp)
@@ -14,7 +13,7 @@ function cargarEventListeners(){
     asunto.addEventListener("blur",validarFormulario)
     mensaje.addEventListener("blur",validarFormulario)
     formulario.addEventListener("submit",enviarEmail)
-    btnReset.addEventListener("click",function(e){
+    btnReset.addEventListener("click",(e)=>{
         e.preventDefault()
         resetearFormulario()
     })
@@ -26,7 +25,7 @@ function iniciarApp(){
 }
 
 function validarFormulario(e){
-    if(e.target.value.length > 1){
+    if(e.target.value.length > 0){
         const error = document.querySelector(".error")
         if(error){
             error.remove()
@@ -38,6 +37,7 @@ function validarFormulario(e){
         e.target.classList.add("border","border-red-500")
         mostrarError("Todos los campos son obligatorios")
     }
+
     if(e.target.type === "email"){
         if(er.test(e.target.value)){
             const error = document.querySelector(".error")
@@ -53,7 +53,7 @@ function validarFormulario(e){
         }
     }
 
-    if(er.test(email.value) && asunto.value !== "" && mensaje.value !== ""){
+    if(er.test(email.value) && mensaje.value !== "" && asunto.value !== ""){
         btnEnviar.disabled = false
         btnEnviar.classList.remove("cursor-not-allow","opacity-50")
     }
@@ -63,6 +63,7 @@ function mostrarError(mensaje){
     const mensajeError = document.createElement("p")
     mensajeError.textContent = mensaje
     mensajeError.classList.add("border","border-red-500","bg-red-300","text-black","p-3","text-center","mt-5","error","font-bold","uppercase")
+
     const errores = document.querySelectorAll(".error")
     if(errores.length === 0){
         formulario.appendChild(mensajeError)
@@ -73,13 +74,13 @@ function enviarEmail(e){
     e.preventDefault()
     const spinner = document.querySelector("#spinner")
     spinner.style.display = "flex"
-
     setTimeout(() => {
         spinner.style.display = "none"
         const parrafo = document.createElement("p")
-        parrafo.textContent = "Mensaje Enviado Correctamente"
+        parrafo.textContent = "Mensaje enviado correctamente"
         parrafo.classList.add("border","border-green-500","bg-green-300","text-black","p-3","text-center","mt-5","error","font-bold","uppercase")
         formulario.appendChild(parrafo)
+
         setTimeout(() => {
             parrafo.remove()
             resetearFormulario()
