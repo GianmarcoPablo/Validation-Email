2bbffe005d8ce54d1353765f35fce72f442c40e4
Gian Marco Pablo Totocayo <gianmarcopablo588@gmail.com>
2023-03-31 22:34:34 -0500

    actualizacion

--- js/testing4.js
+++ js/testing4.js
@@ -6,14 +6,14 @@ const formulario = document.querySelector("#enviar-mail")
 const er = /^(([^<>()\[\]\\.,;:\s@\"]+(\.[^<>()\[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 const btnReset = document.querySelector("#resetBtn")
 
-cargarEvenlisteners()
-function cargarEvenlisteners(){
-    document.addEventListener("DOMContentLoaded", iniciarApp)
-    email.addEventListener("blur", validarFormulario)
-    asunto.addEventListener("blur", validarFormulario)
-    mensaje.addEventListener("blur", validarFormulario)
-    formulario.addEventListener("submit", enviarEmail)
-    btnReset.addEventListener("click", function(e){
+cargarEventListeners()
+function cargarEventListeners(){
+    document.addEventListener("DOMContentLoaded",iniciarApp)
+    email.addEventListener("blur",validarFormulario)
+    asunto.addEventListener("blur",validarFormulario)
+    mensaje.addEventListener("blur",validarFormulario)
+    formulario.addEventListener("submit",enviarEmail)
+    btnReset.addEventListener("click",(e)=>{
         e.preventDefault()
         resetearFormulario()
     })
@@ -21,48 +21,48 @@ function cargarEvenlisteners(){
 
 function iniciarApp(){
     btnEnviar.disabled = true
-    btnEnviar.classList.add("cursor-not-allowed", "opacity-50")
+    btnEnviar.classList.add("cursor-not-allow","opacity-50")
 }
 
 function validarFormulario(e){
-    e.preventDefault()
     if(e.target.value.length > 0){
         const error = document.querySelector(".error")
         if(error){
             error.remove()
         }
-        e.target.classList.remove("border", "border-red-500")
-        e.target.classList.add("border", "border-green-500")
+        e.target.classList.remove("border","border-red-500")
+        e.target.classList.add("border","border-green-500")
     }else{
-        e.target.classList.remove("border", "border-green-500")
-        e.target.classList.add("border", "border-red-500")
-        mmostrarError("Todos los campos son obligatorios")
+        e.target.classList.remove("border","border-green-500")
+        e.target.classList.add("border","border-red-500")
+        mostrarError("Todos los campos son obligatorios")
     }
+
     if(e.target.type === "email"){
         if(er.test(e.target.value)){
             const error = document.querySelector(".error")
             if(error){
                 error.remove()
             }
-            e.target.classList.remove("border", "border-red-500")
-            e.target.classList.add("border", "border-green-500")
+            e.target.classList.remove("border","border-red-500")
+            e.target.classList.add("border","border-green-500")
         }else{
-            e.target.classList.remove("border", "border-green-500")
-            e.target.classList.add("border", "border-red-500")
-            mmostrarError("Email no valido")
+            e.target.classList.remove("border","border-green-500")
+            e.target.classList.add("border","border-red-500")
+            mostrarError("Email no valido")
         }
     }
 
     if(er.test(email.value) && asunto.value !== "" && mensaje.value !== ""){
         btnEnviar.disabled = false
-        btnEnviar.classList.remove("cursor-not-allowed", "opacity-50")
+        btnEnviar.classList.remove("cursor-not-allow","opacity-50")
     }
 }
 
-function mmostrarError(mensaje){
+function mostrarError(mensaje){
     const mensajeError = document.createElement("p")
     mensajeError.textContent = mensaje
-    mensajeError.classList.add("border", "border-red-500", "background-red-100", "text-red-500", "p-3", "mt-5", "text-center", "error")
+    mensajeError.classList.add("border","border-red-500","bg-red-300","text-black","p-3","text-center","mt-5","error","font-bold","uppercase")
     const errores = document.querySelectorAll(".error")
     if(errores.length === 0){
         formulario.appendChild(mensajeError)
@@ -73,19 +73,19 @@ function enviarEmail(e){
     e.preventDefault()
     const spinner = document.querySelector("#spinner")
     spinner.style.display = "flex"
-
-    setTimeout(() => {
+    
+    setTimeout(()=>{
         spinner.style.display = "none"
         const parrafo = document.createElement("p")
-        parrafo.textContent = "El mensaje se envio correctamente"
-        parrafo.classList.add("text-center", "my-10", "p-2", "bg-green-500", "text-white", "font-bold", "uppercase")
+        parrafo.textContent = "Mensaje Enviado Correctamente"
+        parrafo.classList.add("border","border-green-500","bg-green-300","text-black","p-3","text-center","mt-5","error","font-bold","uppercase")
         formulario.appendChild(parrafo)
 
         setTimeout(() => {
             parrafo.remove()
-            resetearFormulario()
-        }, 3000)
-    }, 3000)
+            iniciarApp()
+        }, 3000);
+    },3000)
 }
 
 function resetearFormulario(){
